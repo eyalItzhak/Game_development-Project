f64c87fc248ee095880ddb6dc0f45d160d5c02a1
EyalQ-LAB <62293316+eyalItzhak@users.noreply.github.com>
2023-05-01 23:40:38 +0300

    add scary sound

--- Assets/Triggers/gateTrigger.cs
+++ Assets/Triggers/gateTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class gateTrigger : MonoBehaviour
 {
     private Animator mAnimator;
+    public AudioSource playSound;
    
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,7 @@ public class gateTrigger : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("out");
+        playSound.Play();
     }
 
     private void OnTriggerStay(Collider other)
